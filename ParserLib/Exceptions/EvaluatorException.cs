76a8dab964fa9b7f105c57ec456a925bd2176b10
Joël Luijmes <joelluijmes@gmail.com>
2016-09-14 13:36:48 +0200

    added EvaluatorException

---
+++ ParserLib/Exceptions/EvaluatorException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ParserLib
+{
+    public sealed class EvaluatorException : Exception
+    {
+        public EvaluatorException()
+        {
+        }
+
+        public EvaluatorException(string message) : base(message)
+        {
+        }
+
+        public EvaluatorException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
