386a42c45c01dbce738ff1817a2058654801ab9c
Joël Luijmes <joelluijmes@gmail.com>
2016-10-24 11:26:43 +0200

    added MatchWhile; MatchAnyChar

--- ParserLib.Tests/GrammarTest.cs
+++ ParserLib.Tests/GrammarTest.cs
@@ -5,218 +5,233 @@ using ParserLib.Parsing.Rules;
 
 namespace ParserLib.Tests
 {
-    [TestFixture]
-    public sealed class GrammarTests
-    {
-        [Test]
-        public void TestAnyCaseInsensitiveString()
-        {
-            var rule = Grammar.MatchAnyString("dog cat fish", true);
+	[TestFixture]
+	public sealed class GrammarTests
+	{
+		[Test]
+		public void TestAnyCaseInsensitiveString()
+		{
+			var rule = Grammar.MatchAnyString("dog cat fish", true);
 
-            Assert.IsTrue(rule.Match("dOgfish"));
-            Assert.IsTrue(rule.Match("caTfish"));
-            Assert.IsTrue(rule.Match("Fishfish"));
-            Assert.IsFalse(rule.Match("tacdog"));
-        }
+			Assert.IsTrue(rule.Match("dOgfish"));
+			Assert.IsTrue(rule.Match("caTfish"));
+			Assert.IsTrue(rule.Match("Fishfish"));
+			Assert.IsFalse(rule.Match("tacdog"));
+		}
 
-        [Test]
-        public void TestAnyChar()
-        {
-            var rule = Grammar.MatchAnyChar('a', 'b', 'c', 'd');
+		[Test]
+		public void TestAnyChar()
+		{
+			var rule = Grammar.MatchAnyChar('a', 'b', 'c', 'd');
 
-            Assert.IsTrue(rule.Match("a"));
-            Assert.IsTrue(rule.Match("b"));
-            Assert.IsTrue(rule.Match("d"));
-            Assert.IsFalse(rule.Match("q"));
-        }
+			Assert.IsTrue(rule.Match("a"));
+			Assert.IsTrue(rule.Match("b"));
+			Assert.IsTrue(rule.Match("d"));
+			Assert.IsFalse(rule.Match("q"));
 
-        [Test]
-        public void TestAnyString()
-        {
-            var rule = Grammar.MatchAnyString("dog cat fish");
+			rule = Grammar.MatchAnyChar();
+			Assert.IsTrue(rule.Match("a"));
+			Assert.IsTrue(rule.Match("x"));
+			Assert.Throws<ParserException>(() => rule.Match(""));
+		}
 
-            Assert.IsTrue(rule.Match("dogfish"));
-            Assert.IsTrue(rule.Match("catfish"));
-            Assert.IsTrue(rule.Match("fishfish"));
-            Assert.IsFalse(rule.Match("tacdog"));
-        }
+		[Test]
+		public void TestAnyString()
+		{
+			var rule = Grammar.MatchAnyString("dog cat fish");
 
-        [Test]
-        public void TestBinaryRule()
-        {
-            var left = Grammar.Node("letter", Grammar.Char(char.IsLetter));
-            var right = Grammar.Node("digit", Grammar.Char(char.IsDigit));
-            var add = Grammar.Node("add", Grammar.MatchAnyString("+ add"));
-            var sub = Grammar.Node("sub", Grammar.MatchAnyString("- sub"));
-            var and = Grammar.Node("and", Grammar.MatchAnyString("& and"));
-            var or = Grammar.Node("or", Grammar.MatchAnyString("| or"));
-            var op = add | sub | and | or;
+			Assert.IsTrue(rule.Match("dogfish"));
+			Assert.IsTrue(rule.Match("catfish"));
+			Assert.IsTrue(rule.Match("fishfish"));
+			Assert.IsFalse(rule.Match("tacdog"));
+		}
 
-            var ruleFixed = Grammar.Binary(left, op, right, true);
-            Assert.IsTrue(ruleFixed.Match("k+2"));
-            Assert.IsFalse(ruleFixed.Match("2+k"));
+		[Test]
+		public void TestBinaryRule()
+		{
+			var left = Grammar.Node("letter", Grammar.Char(char.IsLetter));
+			var right = Grammar.Node("digit", Grammar.Char(char.IsDigit));
+			var add = Grammar.Node("add", Grammar.MatchAnyString("+ add"));
+			var sub = Grammar.Node("sub", Grammar.MatchAnyString("- sub"));
+			var and = Grammar.Node("and", Grammar.MatchAnyString("& and"));
+			var or = Grammar.Node("or", Grammar.MatchAnyString("| or"));
+			var op = add | sub | and | or;
 
-            var ruleUnfixed = Grammar.Binary(left, op, right);
-            Assert.IsTrue(ruleUnfixed.Match("k+2"));
-            Assert.IsTrue(ruleUnfixed.Match("2+k"));
-        }
+			var ruleFixed = Grammar.Binary(left, op, right, true);
+			Assert.IsTrue(ruleFixed.Match("k+2"));
+			Assert.IsFalse(ruleFixed.Match("2+k"));
 
-        [Test]
-        public void TestChar()
-        {
-            var rule = Grammar.Char(char.IsDigit);
+			var ruleUnfixed = Grammar.Binary(left, op, right);
+			Assert.IsTrue(ruleUnfixed.Match("k+2"));
+			Assert.IsTrue(ruleUnfixed.Match("2+k"));
+		}
 
-            Assert.IsTrue(rule.Match("1"));
-            Assert.IsFalse(rule.Match("a"));
-        }
+		[Test]
+		public void TestChar()
+		{
+			var rule = Grammar.Char(char.IsDigit);
 
-        [Test]
-        public void TestEndRule()
-        {
-            var rule = Grammar.End();
+			Assert.IsTrue(rule.Match("1"));
+			Assert.IsFalse(rule.Match("a"));
+		}
 
-            Assert.IsTrue(rule.Match(""));
-            Assert.IsFalse(rule.Match("something"));
-        }
+		[Test]
+		public void TestEndRule()
+		{
+			var rule = Grammar.End();
 
-        [Test]
-        public void TestMatchChar()
-        {
-            var rule = Grammar.MatchChar('a');
+			Assert.IsTrue(rule.Match(""));
+			Assert.IsFalse(rule.Match("something"));
+		}
 
-            Assert.IsTrue(rule.Match("a"));
-            Assert.IsFalse(rule.Match("A"));
-            Assert.IsFalse(rule.Match("q"));
-        }
+		[Test]
+		public void TestMatchChar()
+		{
+			var rule = Grammar.MatchChar('a');
 
-        [Test]
-        public void TestMatchCharCaseInsensitive()
-        {
-            var rule = Grammar.MatchChar('a', true);
+			Assert.IsTrue(rule.Match("a"));
+			Assert.IsFalse(rule.Match("A"));
+			Assert.IsFalse(rule.Match("q"));
+		}
 
-            Assert.IsTrue(rule.Match("a"));
-            Assert.IsTrue(rule.Match("A"));
-            Assert.IsFalse(rule.Match("q"));
-        }
+		[Test]
+		public void TestMatchCharCaseInsensitive()
+		{
+			var rule = Grammar.MatchChar('a', true);
 
-        [Test]
-        public void TestOneOrMoreRule()
-        {
-            var rule = Grammar.OneOrMore(Grammar.MatchString("test "));
+			Assert.IsTrue(rule.Match("a"));
+			Assert.IsTrue(rule.Match("A"));
+			Assert.IsFalse(rule.Match("q"));
+		}
 
-            Assert.IsTrue(rule.Match("test something"));
-            Assert.IsTrue((rule + Grammar.MatchString("something")).Match("test test something"));
-            Assert.IsFalse(rule.Match("something"));
-        }
+		[Test]
+		public void TestNode()
+		{
+			var rule = Grammar.Node(Grammar.Digit);
 
-        [Test]
-        public void TestOptionalRule()
-        {
-            var rule = Grammar.Optional(Grammar.MatchString("test"));
+			Assert.IsTrue(rule.Match("1"));
+			Assert.IsTrue(rule.Match("2"));
+			Assert.IsFalse(rule.Match("x"));
+			Assert.Throws<ArgumentNullException>(() => Grammar.Node(null));
+		}
 
-            Assert.IsTrue(rule.Match("test"));
-            Assert.IsTrue(rule.Match("something"));
-        }
+		[Test]
+		public void TestOneOrMoreRule()
+		{
+			var rule = Grammar.OneOrMore(Grammar.MatchString("test "));
 
-        [Test]
-        public void TestOrRule()
-        {
-            var rule = Grammar.Or(Grammar.MatchString("cat"), Grammar.MatchString("dog"));
+			Assert.IsTrue(rule.Match("test something"));
+			Assert.IsTrue((rule + Grammar.MatchString("something")).Match("test test something"));
+			Assert.IsFalse(rule.Match("something"));
+		}
 
-            Assert.IsFalse(rule.Match("something"));
-            Assert.IsFalse(rule.Match("fishcatdog"));
-            Assert.IsTrue(rule.Match("catfish"));
-            Assert.IsTrue(rule.Match("dogfish"));
-        }
+		[Test]
+		public void TestOptionalRule()
+		{
+			var rule = Grammar.Optional(Grammar.MatchString("test"));
 
-        [Test]
-        public void TestRecursive()
-        {
-            var op = Grammar.MatchAnyString("+ -");
-            var digit = new RegexRule("\\d+");
+			Assert.IsTrue(rule.Match("test"));
+			Assert.IsTrue(rule.Match("something"));
+		}
 
-            Rule expressionA = null;
-            var recursiveExpression = Grammar.Func(() => expressionA);
+		[Test]
+		public void TestOrRule()
+		{
+			var rule = Grammar.Or(Grammar.MatchString("cat"), Grammar.MatchString("dog"));
 
-            var expression = digit + recursiveExpression;
-            expressionA = (op + digit + recursiveExpression) | Grammar.End();
+			Assert.IsFalse(rule.Match("something"));
+			Assert.IsFalse(rule.Match("fishcatdog"));
+			Assert.IsTrue(rule.Match("catfish"));
+			Assert.IsTrue(rule.Match("dogfish"));
+		}
 
-            Assert.IsTrue(expression.Match("1"));
-            Assert.IsTrue(expression.Match("1+2"));
-            Assert.IsTrue(expression.Match("1+2+3"));
-        }
+		[Test]
+		public void TestRecursive()
+		{
+			var op = Grammar.MatchAnyString("+ -");
+			var digit = new RegexRule("\\d+");
 
-        [Test]
-        public void TestRegexRule()
-        {
-            var rule = Grammar.Regex("\\d");
+			Rule expressionA = null;
+			var recursiveExpression = Grammar.Func(() => expressionA);
 
-            Assert.IsTrue(rule.Match("1"));
-            Assert.IsTrue(rule.Match("123"));
-            Assert.IsFalse(rule.Match("test"));
-        }
+			var expression = digit + recursiveExpression;
+			expressionA = (op + digit + recursiveExpression) | Grammar.End();
 
-        [Test]
-        public void TestSequenceRule()
-        {
-            var rule = Grammar.Sequence(Grammar.MatchString("cat"), Grammar.MatchString("fish"));
+			Assert.IsTrue(expression.Match("1"));
+			Assert.IsTrue(expression.Match("1+2"));
+			Assert.IsTrue(expression.Match("1+2+3"));
+		}
 
-            Assert.IsFalse(rule.Match("catsomething"));
-            Assert.IsFalse(rule.Match("fishcat"));
-            Assert.IsTrue(rule.Match("catfish"));
-            Assert.IsTrue(rule.Match("catfish something"));
-        }
+		[Test]
+		public void TestRegexRule()
+		{
+			var rule = Grammar.Regex("\\d");
 
-        [Test]
-        public void TestStartRule()
-        {
-            var rule = Grammar.Start();
-            var randomRule = Grammar.MatchString("Test");
+			Assert.IsTrue(rule.Match("1"));
+			Assert.IsTrue(rule.Match("123"));
+			Assert.IsFalse(rule.Match("test"));
+		}
 
-            Assert.IsTrue(rule.Match("sometihng"));
-            Assert.IsFalse((randomRule + rule).Match("Test dingen"));
-        }
+		[Test]
+		public void TestSequenceRule()
+		{
+			var rule = Grammar.Sequence(Grammar.MatchString("cat"), Grammar.MatchString("fish"));
 
-        [Test]
-        public void TestStringCaseInsensitiveRule()
-        {
-            var rule = Grammar.MatchString("Test", true);
+			Assert.IsFalse(rule.Match("catsomething"));
+			Assert.IsFalse(rule.Match("fishcat"));
+			Assert.IsTrue(rule.Match("catfish"));
+			Assert.IsTrue(rule.Match("catfish something"));
+		}
 
-            Assert.IsTrue(rule.Match("Test"));
-            Assert.IsTrue(rule.Match("Test123"));
-            Assert.IsTrue(rule.Match("test123"));
-            Assert.IsFalse(rule.Match("Failing Test"));
-        }
+		[Test]
+		public void TestStartRule()
+		{
+			var rule = Grammar.Start();
+			var randomRule = Grammar.MatchString("Test");
 
-        [Test]
-        public void TestStringRule()
-        {
-            var rule = Grammar.MatchString("Test");
+			Assert.IsTrue(rule.Match("sometihng"));
+			Assert.IsFalse((randomRule + rule).Match("Test dingen"));
+		}
 
-            Assert.IsTrue(rule.Match("Test"));
-            Assert.IsTrue(rule.Match("Test123"));
-            Assert.IsFalse(rule.Match("test123"));
-            Assert.IsFalse(rule.Match("Failing Test"));
-        }
+		[Test]
+		public void TestStringCaseInsensitiveRule()
+		{
+			var rule = Grammar.MatchString("Test", true);
 
-        [Test]
-        public void TestZeroOrMoreRule()
-        {
-            var rule = Grammar.ZeroOrMore(Grammar.MatchString("test"));
+			Assert.IsTrue(rule.Match("Test"));
+			Assert.IsTrue(rule.Match("Test123"));
+			Assert.IsTrue(rule.Match("test123"));
+			Assert.IsFalse(rule.Match("Failing Test"));
+		}
 
-            Assert.IsTrue(rule.Match("something"));
-            Assert.IsTrue(rule.Match("test test something"));
-        }
+		[Test]
+		public void TestStringRule()
+		{
+			var rule = Grammar.MatchString("Test");
 
-        [Test]
-        public void TestNode()
-        {
-            var rule = Grammar.Node(Grammar.Digit);
+			Assert.IsTrue(rule.Match("Test"));
+			Assert.IsTrue(rule.Match("Test123"));
+			Assert.IsFalse(rule.Match("test123"));
+			Assert.IsFalse(rule.Match("Failing Test"));
+		}
 
-            Assert.IsTrue(rule.Match("1"));
-            Assert.IsTrue(rule.Match("2"));
-            Assert.IsFalse(rule.Match("x"));
-            Assert.Throws<ArgumentNullException>(() => Grammar.Node(null));
-        }
-    }
+		[Test]
+		public void TestWhile()
+		{
+			var rule = Grammar.MatchWhile(Grammar.Char(char.IsDigit));
+
+			Assert.IsTrue(rule.Match("123"));
+			Assert.IsTrue(rule.Match("123Test"));
+			Assert.IsFalse(rule.Match("test123"));
+		}
+
+		[Test]
+		public void TestZeroOrMoreRule()
+		{
+			var rule = Grammar.ZeroOrMore(Grammar.MatchString("test"));
+
+			Assert.IsTrue(rule.Match("something"));
+			Assert.IsTrue(rule.Match("test test something"));
+		}
+	}
 }
--- ParserLib/Parsing/Grammar.cs
+++ ParserLib/Parsing/Grammar.cs
@@ -6,44 +6,46 @@ using ParserLib.Parsing.Rules;
 
 namespace ParserLib.Parsing
 {
-    public abstract partial class Grammar
-    {
-        private static readonly Rule _endRule = new EndRule();
-        private static readonly Rule _startRule = new StartRule();
+	public abstract partial class Grammar
+	{
+		private static readonly Rule _endRule = new EndRule();
+		private static readonly Rule _startRule = new StartRule();
 
-        public static Rule Node(Rule rule) => new NodeRule(rule);
-        public static Rule Node(string name, Rule rule) => new NodeRule(name, rule);
-        public static Rule Not(Rule rule) => new NotRule(rule);
-        public static Rule Sequence(Rule firstRule, Rule secondRule, params Rule[] moreRules) => new SequenceRule(firstRule, secondRule, moreRules);
-        public static Rule Sequence(IEnumerable<Rule> rules) => new SequenceRule(rules);
-        public static Rule Or(Rule firstRule, Rule secondRule, params Rule[] moreRules) => new OrRule(firstRule, secondRule, moreRules);
-        public static Rule Or(IEnumerable<Rule> rules) => new OrRule(rules);
-        public static Rule Start() => _startRule;
-        public static Rule End() => _endRule;
-        public static Rule OneOrMore(Rule rule) => new OneOrMoreRule(rule);
-        public static Rule ZeroOrMore(Rule rule) => new ZeroOrMoreRule(rule);
-        public static Rule Optional(Rule rule) => new OptionalRule(rule);
-        public static Rule Func(Func<Rule> ruleFunc) => new FuncRule(ruleFunc);
-        public static Rule Regex(Regex regex) => new RegexRule(regex);
-        public static Rule Regex(string pattern) => new RegexRule(pattern);
-        public static Rule Char(Predicate<char> predicate) => new CharRule(predicate);
-        public static Rule MatchAnyChar(char c, params char[] chars) => Or(Util.MergeArray(c, chars).Select(r => MatchChar(r)));
-        public static Rule MatchString(string pattern, bool ignoreCase = false) => new StringRule(pattern, ignoreCase);
-        public static Rule MatchAnyString(string input, bool ignoreCase = false) => MatchAnyString(input.Split(' '), ignoreCase);
-        public static Rule MatchAnyString(string[] inputs, bool ignoreCase = false) => Or(inputs.Select(s => MatchString(s, ignoreCase)));
-        public static Rule MatchEnum<TEnum>() => MatchAnyString(Enum.GetNames(typeof(TEnum)));
+		public static Rule Node(Rule rule) => new NodeRule(rule);
+		public static Rule Node(string name, Rule rule) => new NodeRule(name, rule);
+		public static Rule Not(Rule rule) => new NotRule(rule);
+		public static Rule Sequence(Rule firstRule, Rule secondRule, params Rule[] moreRules) => new SequenceRule(firstRule, secondRule, moreRules);
+		public static Rule Sequence(IEnumerable<Rule> rules) => new SequenceRule(rules);
+		public static Rule Or(Rule firstRule, Rule secondRule, params Rule[] moreRules) => new OrRule(firstRule, secondRule, moreRules);
+		public static Rule Or(IEnumerable<Rule> rules) => new OrRule(rules);
+		public static Rule Start() => _startRule;
+		public static Rule End() => _endRule;
+		public static Rule OneOrMore(Rule rule) => new OneOrMoreRule(rule);
+		public static Rule ZeroOrMore(Rule rule) => new ZeroOrMoreRule(rule);
+		public static Rule Optional(Rule rule) => new OptionalRule(rule);
+		public static Rule Func(Func<Rule> ruleFunc) => new FuncRule(ruleFunc);
+		public static Rule Regex(Regex regex) => new RegexRule(regex);
+		public static Rule Regex(string pattern) => new RegexRule(pattern);
+		public static Rule Char(Predicate<char> predicate) => new CharRule(predicate);
+		public static Rule MatchAnyChar() => Char(i => true);
+		public static Rule MatchAnyChar(char c, params char[] chars) => Or(Util.MergeArray(c, chars).Select(r => MatchChar(r)));
+		public static Rule MatchString(string pattern, bool ignoreCase = false) => new StringRule(pattern, ignoreCase);
+		public static Rule MatchAnyString(string input, bool ignoreCase = false) => MatchAnyString(input.Split(' '), ignoreCase);
+		public static Rule MatchAnyString(string[] inputs, bool ignoreCase = false) => Or(inputs.Select(s => MatchString(s, ignoreCase)));
+		public static Rule MatchWhile(Rule rule) => OneOrMore(rule + MatchAnyChar());
+		public static Rule MatchEnum<TEnum>() => MatchAnyString(Enum.GetNames(typeof(TEnum)));
 
-        public static Rule MatchChar(char c, bool ignoreCase = false)
-        {
-            var rule = new CharRule(x => ignoreCase
-                ? char.ToLower(x) == char.ToLower(c)
-                : x == c) {Name = $"'{c}'"};
+		public static Rule MatchChar(char c, bool ignoreCase = false)
+		{
+			var rule = new CharRule(x => ignoreCase
+				? char.ToLower(x) == char.ToLower(c)
+				: x == c) {Name = $"'{c}'"};
 
-            return rule;
-        }
+			return rule;
+		}
 
-        public static Rule Binary(Rule left, Rule op, Rule right, bool fixedOrder = false) => fixedOrder
-            ? left + op + right
-            : (left + op + right) | (right + op + left);
-    }
+		public static Rule Binary(Rule left, Rule op, Rule right, bool fixedOrder = false) => fixedOrder
+			? left + op + right
+			: (left + op + right) | (right + op + left);
+	}
 }
